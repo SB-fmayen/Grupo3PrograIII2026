a96a21a9ff968b043d0b05f3d27f65d971761f22
Jose Pablo Lopez Lara <jlopezl107@miumg.edu.gt>
2026-03-17 22:58:46 -0600

    se creo archivo multiplicacion

---
+++ src/main/java/operaciones/Multiplicacion.java
@@ -0,0 +1,22 @@
+
+package operaciones;
+
+import listas.ListaDouble;
+import listas.NodoDouble;
+
+public class Multiplicacion {
+    
+        @Override
+    public double ejecutar(ListaDouble valores) {
+    if (valores.estaVacia()) return 0;
+    double resultado = 1.0;
+    NodoDouble actual = valores.getCabeza();
+    while (actual != null) {
+    resultado *= actual.valor;
+    actual = actual.siguiente;
+    }
+    return resultado;
+    }
+    
+}
+
