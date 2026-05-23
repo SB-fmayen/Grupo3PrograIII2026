4177263b0004aa26f15c90d18bb6ea3eb50b7e4d
brayan-castellon <bcastellonm@miumg.edu.gt>
2026-05-22 20:41:00 -0600

    avance

---
+++ src/main/java/manejoMemoria/proceso.java
@@ -0,0 +1,27 @@
+ package manejoMemoria;
+
+/**
+ *
+ */
+public class proceso {
+    String nombre;
+    int tamaño;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+    
+    
+}
